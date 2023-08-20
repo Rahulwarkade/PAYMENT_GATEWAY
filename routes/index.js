fa0bdb6fba440694b4b9205f48c6de0ca779ba36
Rahul <rahulwarkade954@gmail.com>
2023-08-21 01:58:04 +0530

    Payment verification

--- routes/index.js
+++ routes/index.js
@@ -22,6 +22,18 @@ router.post("/create/orderId",function(req,res){
 
   instance.orders.create(options,function(err,order){
     console.log(order)
+    res.send(order);
   })
 }) 
+
+router.post("/api/payment/verify",function(req,res){
+  var { validatePaymentVerification, validateWebhookSignature } = require('../node_modules/razorpay/dist/utils/razorpay-utils');
+  var razorpayOrderId = req.body.response.razorpay_order_id;
+  var razorpayPaymentId = req.body.response.razorpay_payment_id;
+  var signature = req.body.response.razorpay_signature;
+  var secret = "A3Q12Qy1t83qnVooybkx114V";
+  var status = validatePaymentVerification({"order_id": razorpayOrderId, "payment_id": razorpayPaymentId }, signature, secret);
+
+  res.send({signatureIsValid : status});
+})
 module.exports = router; 
